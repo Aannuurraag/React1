fc6dfd97ef95eee9495e98021e301bbcb991b5f3
Anurag Chaurasia <anuragchaurasia9619@gmail.com>
2025-04-17 15:14:21 +0530

    first commit

--- App.js
+++ App.js
@@ -1,19 +1,15 @@
-let parent=React.createElement(
-  "div",
-  { id: "parent" },
-  [
-    React.createElement("div", { id: "child1" }, [
-      React.createElement("h1", {}, "this is an h1 tag"),
-      React.createElement("h2", {}, "this is an h2 tag"),
-    ]),
-  ],
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "this is an h1 tag"),
-    React.createElement("h2", {}, "this is an h2 tag"),
-  ])
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-let root= ReactDOM.createRoot(document.getElementById("root"))
+const Header=()=>{
+ return <div className="header">
+    <div className="header-section logo">LOGO</div>
+    <div className="header-section search-bar"><input type="text" placeholder="Search"></input> </div>
+    <div className="header-section user-icon">User icon</div>
+  </div>
+}
+   
 
-root.render(parent)
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
+root.render(<Header/>);
